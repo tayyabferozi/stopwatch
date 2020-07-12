070ef3ca5038aeb1afa0b657b7b7fcddae4e7108
= <=>
2020-07-12 02:00:45 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+var startBtn = document.querySelector("#start");
+var stopBtn = document.querySelector("#stop");
+var lapBtn = document.querySelector("#lap");
+
+var secondHolder = document.querySelector(".second-holder");
+var minuteHolder = document.querySelector(".minute-holder");
+var hourHolder = document.querySelector(".hour-holder");
+
+var timerInterval;
+
+function updateTime() {
+  var secondInt = +secondHolder.innerHTML + 1;
+  var minuteInt = +minuteHolder.innerHTML;
+  var hourInt = +hourHolder.innerHTML;
+
+  if (secondInt === 60) {
+    minuteInt++;
+    secondInt = 0;
+  }
+
+  if (minuteInt === 60) {
+    minuteInt = 0;
+    hourInt++;
+  }
+
+  var formattedSecond = secondInt > 9 ? "" + secondInt : "0" + secondInt;
+  secondHolder.innerHTML = formattedSecond;
+
+  var formattedMinute = minuteInt > 9 ? "" + minuteInt : "0" + minuteInt;
+  minuteHolder.innerHTML = formattedMinute;
+
+  var formattedHour = hourInt > 9 ? "" + hourInt : "0" + hourInt;
+  hourHolder.innerHTML = formattedHour;
+}
+
+startBtn.addEventListener("click", function () {
+  timerInterval = setInterval(updateTime, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+});
+
+stopBtn.addEventListener("click", function () {
+  clearInterval(timerInterval);
+  stopBtn.disabled = true;
+  startBtn.disabled = false;
+});
